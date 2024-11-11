af7d72133f06f2be25fa8720aad6e15b74bf274a
1amsahil <sahil981156@gmail.com>
2024-11-11 19:19:33 +0530

    XML Config File (resources)

---
+++ src/main/java/com/Eternity/code/Programmer.java
@@ -0,0 +1,14 @@
+package com.Eternity.code;
+
+public class Programmer {
+
+    public Programmer()
+    {
+        System.out.println("Object Created ");
+    }
+
+    public void code()
+    {
+        System.out.println("Coding");
+    }
+}
