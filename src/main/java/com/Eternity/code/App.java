352de409ceb8f218fa8a9daa7e08be1192e3dce9
1amsahil <sahil981156@gmail.com>
2024-11-11 20:41:12 +0530

    Setter Method called through Xml Property Tag

--- src/main/java/com/Eternity/code/App.java
+++ src/main/java/com/Eternity/code/App.java
@@ -10,10 +10,6 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 
         Programmer obj = (Programmer) context.getBean("programmer");
-        obj.age = 23;
-        System.out.println(obj.age);
-
-        Programmer newObj = (Programmer) context.getBean("programmer");
-        System.out.println(newObj.age);
+        System.out.println("Age : "+obj.getAge());
     }
 }
--- src/main/java/com/Eternity/code/Programmer.java
+++ src/main/java/com/Eternity/code/Programmer.java
@@ -6,7 +6,17 @@ public class Programmer {
     {
         System.out.println("Object Created ");
     }
-    int age;
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
+    }
 
     public void code()
     {
